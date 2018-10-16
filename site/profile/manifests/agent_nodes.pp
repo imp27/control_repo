df5f1b3782912b8ff5ffdc8dfd7f9e3a597c58bf
imp27 <43071180+imp27@users.noreply.github.com>
2018-10-16 10:46:56 +0100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include dockeragent
-  dockeragent::node {'puppet'}
-  dockeragent::node {'db'}
+  dockeragent::node {'puppet.mshome.net'}
+  dockeragent::node {'db.mshome.net'}
 }
